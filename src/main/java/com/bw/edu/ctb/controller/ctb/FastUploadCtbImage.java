f779e8669236055fa33f3f7238588550a91b1264
在晨 <zaichen@zaichen.local>
2020-04-23 12:06:54 +0800

    fu

---
+++ src/main/java/com/bw/edu/ctb/controller/ctb/FastUploadCtbImage.java
@@ -0,0 +1,102 @@
+package com.bw.edu.ctb.controller.ctb;
+
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/myctb/fuci")
+public class FastUploadCtbImage {
+    /*
+     * 获取file.html页面
+     */
+    @RequestMapping("file")
+    public String file(){
+        return "/file";
+    }
+
+    /**
+     * 实现文件上传
+     * */
+    @RequestMapping("fu")
+    @ResponseBody
+    public String fileUpload(@RequestParam("fileName") MultipartFile file){
+        if(file.isEmpty()){
+            return "false";
+        }
+        String fileName = file.getOriginalFilename();
+        int size = (int) file.getSize();
+        System.out.println(fileName + "-->" + size);
+
+        String path = "F:/test" ;
+        File dest = new File(path + "/" + fileName);
+        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest); //保存文件
+            return "true";
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "false";
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "false";
+        }
+    }
+
+    /**
+     * 获取multifile.html页面
+     */
+    @RequestMapping("multifile")
+    public String multifile(){
+        return "/multifile";
+    }
+
+    /**
+     * 实现多文件上传
+     * */
+    /**public @ResponseBody String multifileUpload(@RequestParam("fileName")List<MultipartFile> files) */
+    @RequestMapping(value="multifileUpload",method= RequestMethod.POST)
+    public @ResponseBody String multifileUpload(HttpServletRequest request){
+
+        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("fileName");
+
+        if(files.isEmpty()){
+            return "false";
+        }
+
+        String path = "F:/test" ;
+
+        for(MultipartFile file:files){
+            String fileName = file.getOriginalFilename();
+            int size = (int) file.getSize();
+            System.out.println(fileName + "-->" + size);
+
+            if(file.isEmpty()){
+                return "false";
+            }else{
+                File dest = new File(path + "/" + fileName);
+                if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+                    dest.getParentFile().mkdir();
+                }
+                try {
+                    file.transferTo(dest);
+                }catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return "false";
+                }
+            }
+        }
+        return "true";
+    }
+}
